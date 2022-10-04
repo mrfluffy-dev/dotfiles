2fc4c45890abad6e84f381218db2b9ebbc2f2891
Zastian Pretorius <zastian00@gmail.com>
2022-10-04 22:11:27 +0100

    more

--- script/.config/script/waylock.sh
+++ script/.config/script/waylock.sh
@@ -2,4 +2,4 @@
 
 swayidle -w \
     timeout 600 'systemctl suspend' \
-    before-sleep 'swaylock -i $HOME/Pictures/Wallpapers/130.jpg --ring-color 8218c4 --key-hl-color 3e0c60'
+    before-sleep 'swaylock -i $HOME/Pictures/Wallpapers/133.png --ring-color 8218c4 --key-hl-color 3e0c60'
